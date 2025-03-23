3ae9dd97dba2b01685f8a0fee8e689e99de1eed7
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-03-23 06:19:54 -0700

    liker

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+const modalMessage = document.getElementById('modal-message');
+const hearts = document.querySelectorAll('.like-glyph');
 
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+    .then(() => {
+      // If server call is successful
+      if (heart.textContent === EMPTY_HEART) {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      } else {
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      }
+    })
+    .catch((error) => {
+      // On failure, show error modal and message
+      modal.classList.remove('hidden');
+      modalMessage.textContent = error;
+      // Hide the modal after 3 seconds
+      setTimeout(() => {
+        modal.classList.add('hidden');
+      }, 3000);
+    });
+});
+});
 
 
 
@@ -23,3 +50,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
